839428b3808a550f0d0fba7d5d51f4bfac94cc8b
riqardos <kastiak.richard@gmail.com>
2022-12-11 11:36:57 +0100

    removed styles

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -96,9 +96,6 @@ const getThemeModePalette = (mode: PaletteMode) => ({
 					flexDirection: 'column',
 					minHeight: '100vh'
 				},
-				'html, body, #root': {
-					height: '100%'
-				},
 				'ul': {
 					listStyle: 'none',
 					paddingInlineStart: 0
